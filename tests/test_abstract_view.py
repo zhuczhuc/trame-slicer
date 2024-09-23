159101c330c43de9eafdc1cec01dd0c12b98ee82
Thibault Pelletier <thibault.pelletier@kitware.com>
2024-09-23 14:22:37 +0200

    fix(test): Fix factory tests

--- tests/test_abstract_view.py
+++ tests/test_abstract_view.py
@@ -1,4 +1,3 @@
-import pytest
 import vtk
 from vtkmodules.vtkMRMLDisplayableManager import (
     vtkMRMLCameraDisplayableManager,
@@ -26,9 +25,6 @@ def test_abstract_view_can_render_a_simple_cone(a_slicer_app):
     view.render()
 
 
-@pytest.mark.xfail(
-    reason="VTK Object Factory :: Create Instance not available for SlicerLib yet"
-)
 def test_displayable_manager_group_can_use_displayable_string_instantiation():
     ruler_displayable_manager = (
         vtkMRMLDisplayableManagerGroup.InstantiateDisplayableManager(
@@ -38,10 +34,7 @@ def test_displayable_manager_group_can_use_displayable_string_instantiation():
     assert ruler_displayable_manager is not None
 
 
-@pytest.mark.xfail(
-    reason="VTK Object Factory :: Create Instance not available for SlicerLib yet"
-)
-def test_displayable_group_can_be_initialized_by_factories():
+def test_displayable_group_can_be_initialized_by_factories(a_slicer_app):
     managers = [
         vtkMRMLVolumeRenderingDisplayableManager,
         vtkMRMLCameraDisplayableManager,
@@ -49,11 +42,14 @@ def test_displayable_group_can_be_initialized_by_factories():
 
     # Don't use singleton for test
     factory = vtkMRMLThreeDViewDisplayableManagerFactory()
+    factory.SetMRMLApplicationLogic(a_slicer_app.app_logic)
     for manager in managers:
         if not factory.IsDisplayableManagerRegistered(manager.__name__):
             assert factory.RegisterDisplayableManager(manager.__name__)
 
     renderer = vtk.vtkRenderer()
+    render_window = vtk.vtkRenderWindow()
+    render_window.AddRenderer(renderer)
     displayable_manager_group = vtkMRMLDisplayableManagerGroup()
     displayable_manager_group.Initialize(factory, renderer)
     assert displayable_manager_group.GetDisplayableManagerCount() == 2
